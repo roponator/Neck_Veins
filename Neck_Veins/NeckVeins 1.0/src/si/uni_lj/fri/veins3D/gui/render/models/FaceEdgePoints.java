0a980f4298a1e8ab19ecc617bbc730931e0745ad
Matevž Ropret <ropret.m@gmail.com>
2015-07-11 18:25:20 +0200

    Revert "first"
    This reverts commit edce244fce13b55e14f2f49fd62a783c97626d39.
    
    fg:x
    :x
    xc
    ta/.plugins/org.eclipse.core.resources/.history/12/00e611215627001519b3b4966a504a1e

--- Neck_Veins/NeckVeins 1.0/src/si/uni_lj/fri/veins3D/gui/render/models/FaceEdgePoints.java
+++
@@ -1,5 +0,0 @@
-package si.uni_lj.fri.veins3D.gui.render.models;
-
-public class FaceEdgePoints {
-	int e1 = -1, e2 = -1, e3 = -1;
-}
--- Neck_Veins/NeckVeins 1.0/src/si/uni_lj/fri/veins3D/segmentation/MHDReader.java
+++
@@ -1,6 +0,0 @@
-package si.uni_lj.fri.veins3D.segmentation;
-
-public class MHDReader {
-	public void lala() {
-	}
-}
