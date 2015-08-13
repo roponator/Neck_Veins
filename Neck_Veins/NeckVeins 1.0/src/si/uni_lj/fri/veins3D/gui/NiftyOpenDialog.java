84c9c17bdef88472d074f199ddb98c9e04cf1db4
Matevž Ropret <ropret.m@gmail.com>
2015-08-14 00:28:07 +0200

    file browser almost done

--- Neck_Veins/NeckVeins 1.0/src/si/uni_lj/fri/veins3D/gui/NiftyOpenDialog.java
+++ Neck_Veins/NeckVeins 1.0/src/si/uni_lj/fri/veins3D/gui/NiftyOpenDialog.java
@@ -5,6 +5,8 @@ import de.lessvoid.nifty.controls.ListBoxSelectionChangedEvent;
 import de.lessvoid.nifty.controls.TreeBox;
 import de.lessvoid.nifty.controls.TreeItem;
 import de.lessvoid.nifty.controls.Window;
+import de.lessvoid.nifty.controls.dropdown.DropDownControl;
+import de.lessvoid.nifty.controls.listbox.ListBoxControl;
 import de.lessvoid.nifty.elements.Element;
 import de.lessvoid.nifty.render.NiftyImage;
 import si.uni_lj.fri.veins3D.gui.NiftyFolderBrowser.MyTreeItem;
@@ -23,6 +25,24 @@ public class NiftyOpenDialog
 		// get folder treebox
 		Element treeboxParentPanel = m_mainOpenDailogElement.findElementById("OPEN_DIALOG_FOLDER_TREEBOX_PANEL_CONTAINER");
 		m_folderBrowser = new NiftyFolderBrowser(treeboxParentPanel);
+		
+		
+		Element fileListListboxElement = m_mainOpenDailogElement.findElementById("OPEN_DIALOG_FILES_LIST_LISTBOX");
+		Element fileTypeDropdownElement = m_mainOpenDailogElement.findElementById("OPEN_DIALOG_FILE_TYPE_DROPDOWN");
+
+		de.lessvoid.nifty.controls.ListBox fileListControl = fileListListboxElement.getAttachedInputControl().getControl(ListBoxControl.class);
+		de.lessvoid.nifty.controls.DropDown fileTypeControl = fileTypeDropdownElement.getAttachedInputControl().getControl(DropDownControl.class);
+		
+		String space = " ";
+		fileTypeControl.addItem(space+".mdh");
+		fileTypeControl.addItem(space+".obj");
+		
+		fileListControl.addItem(space+"dfsfdff");
+		fileListControl.addItem(space+"234");
+		fileListControl.addItem(space+"df234ersfdff");
+		fileListControl.addItem(space+"t34");
+		fileListControl.addItem(space+"sdf");
+		
 	}
 
 	public void OnCloseDialog()
